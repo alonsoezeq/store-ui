89116e17e0739f842a99b9e26250df64fc71a27f
Ezequiel Alonso <ealonso@identicum.com>
2021-06-06 20:54:31 -0300

    added error handling and basic validations

--- src/pages/NewProduct.js
+++ src/pages/NewProduct.js
@@ -1,11 +1,19 @@
-import { FormControl, Grid, Input, InputLabel, MenuItem, Select, TextField } from "@material-ui/core"
+import { Button, FormControl, Grid, Input, InputLabel, MenuItem, Select, Snackbar, TextField } from "@material-ui/core"
 import { useState } from "react";
 import useStyles from "./ProductDescription/styles";
 import config from "../config/config";
+import Alert from "@material-ui/lab/Alert";
 
 const NewProduct = () => {
   const classes = useStyles();
-  const [product, setProduct] = useState({
+  
+  const [state, setState] = useState({
+    loading: false,
+    status: null,
+    message: ''
+  });
+
+  const initialProduct = {
     title: '',
     article: '',
     pictures: [],
@@ -13,21 +21,24 @@ const NewProduct = () => {
     color: '#000000',
     category: '',
     description: '',
-    quantity: 0,
-    price: 0
-  });
+    quantity: '',
+    price: ''
+  }
 
+  const [product, setProduct] = useState(initialProduct);
+
+  const {loading, status, message} = state;
   const {title, article, size, color, category, description, quantity, price} = product;
 
-  const changeHandler = (event) => {
+  const handleChange = (event) => {
     setProduct({...product, [event.target.name]: event.target.value});
   }
 
-  const changeNumberHandler = (event) => {
+  const handleNumberChange = (event) => {
     setProduct({...product, [event.target.name]: Number(event.target.value)})
   }
 
-  const changeFileHandler = (event) => {
+  const handleFileChange = (event) => {
     let files = [];
     if (event.target.files.length === 0) {
       setProduct({...product, [event.target.name]: files});
@@ -46,9 +57,15 @@ const NewProduct = () => {
     });
   }
 
-  const submit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     console.log(product);
+
+    setState({
+      loading: true,
+      status: null,
+      message: ''
+    })
 
     fetch(config.baseApi + '/products', {
       method: 'POST',
@@ -59,41 +76,63 @@ const NewProduct = () => {
     })
     .then(res => {
       if (res.ok) {
-        alert("OK!");
+        setState({
+          loading: false,
+          status: 'success',
+          message: 'Producto creado correctamente'
+        });
+        setProduct(initialProduct);
       } else {
-        alert("ERROR!");
+        setState({
+          loading: false,
+          status: 'error',
+          message: 'Error creando el producto'
+        })
       }
+
     })
     .catch(err => {
-      alert("ERROR!");
+      setState({
+        loading: false,
+        status: 'error',
+        message: err.toString()
+      })
     })
   }
 
+  const handleSnackbarClose = () => {
+    setState({
+      loading: false,
+      status: null,
+      message: ''
+    });
+  }
+
   return (
-    <form noValidate autoComplete="off" onSubmit={submit}>
+    <form  autoComplete="off" onSubmit={handleSubmit}>
       <Grid container direction="column">
         <Grid item>
-          <FormControl className={classes.root}>
+          <FormControl required className={classes.root}>
             <InputLabel htmlFor="product">Producto</InputLabel>
-            <Input id="product" name="title" type="text" value={title} onChange={changeHandler} />
+            <Input id="product" name="title" type="text" value={title} onChange={handleChange} />
           </FormControl>
         </Grid>
         <Grid item>
-          <FormControl className={classes.root}>
+          <FormControl required className={classes.root}>
             <InputLabel htmlFor="article">Artículo</InputLabel>
-            <Input id="article" name="article" type="text" value={article} onChange={changeHandler} />
+            <Input id="article" name="article" type="text" value={article} onChange={handleChange} />
           </FormControl>
         </Grid>
         <Grid item>
-          <FormControl className={classes.root}>
+          <FormControl required className={classes.root}>
             <InputLabel htmlFor="pictures">Fotos del producto</InputLabel>
-            <Input id="pictures" name="pictures" type="file" inputProps={{multiple: true, accept: "image/png, image/jpeg"}} onChange={changeFileHandler} />
+            <Input id="pictures" name="pictures" type="file" inputProps={{multiple: true, accept: "image/png, image/jpeg"}} onChange={handleFileChange} />
           </FormControl>
         </Grid>
         <Grid item>
-          <FormControl className={classes.root}>
+          <FormControl required className={classes.root}>
             <InputLabel htmlFor="size">Talle</InputLabel>
-            <Select id="size" name="size" value={size} onChange={changeHandler}>
+            <Select id="size" name="size" value={size} onChange={handleChange}>
               <MenuItem value="XXS">XXS</MenuItem>
               <MenuItem value="XS">XS</MenuItem>
               <MenuItem value="S">S</MenuItem>
@@ -105,41 +144,49 @@ const NewProduct = () => {
           </FormControl>
         </Grid>
         <Grid item>
-          <FormControl className={classes.root}>
+          <FormControl required className={classes.root}>
             <InputLabel htmlFor="color">Color</InputLabel>
-            <Input id="color" name="color" type="color" value={color} onChange={changeHandler} />
+            <Input id="color" name="color" type="color" value={color} onChange={handleChange} />
           </FormControl>
         </Grid>
         <Grid item>
-          <FormControl className={classes.root}>
+          <FormControl required className={classes.root}>
             <InputLabel htmlFor="category">Categoría</InputLabel>
-            <Input id="category" name="category" type="text" value={category} onChange={changeHandler} />
+            <Input id="category" name="category" type="text" value={category} onChange={handleChange} />
           </FormControl>
         </Grid>
         <Grid item>
           <FormControl className={classes.root}>
             <InputLabel htmlFor="description">Descripción</InputLabel>
-            <TextField id="description" name="description" multiline rows={4} value={description} onChange={changeHandler} />
+            <TextField id="description" name="description" multiline rows={4} value={description} onChange={handleChange} />
           </FormControl>
         </Grid>
         <Grid item>
-          <FormControl className={classes.root}>
+          <FormControl required className={classes.root}>
             <InputLabel htmlFor="quantity">Cantidad</InputLabel>
-            <Input id="quantity" name="quantity" type="number" min="1" value={quantity} onChange={changeNumberHandler} />
+            <Input id="quantity" name="quantity" type="number" min="1" value={quantity} onChange={handleNumberChange} />
           </FormControl>
         </Grid>
         <Grid item>
-          <FormControl className={classes.root}>
+          <FormControl required className={classes.root}>
             <InputLabel htmlFor="price">Precio</InputLabel>
-            <Input id="price" name="price" type="number" min="0" value={price} onChange={changeNumberHandler} />
+            <Input id="price" name="price" type="number" min="0" value={price} onChange={handleNumberChange} />
           </FormControl>
         </Grid>
         <Grid item>
           <FormControl className={classes.root}>
-            <Input id="submit" name="submit" type="submit" value="Cargar producto" />
+            <Button id="submit" name="submit" type="submit" disabled={loading}>
+              Cargar producto
+            </Button>
           </FormControl>
         </Grid>
       </Grid>
+      {
+        status && 
+        <Snackbar open autoHideDuration={6000}>
+          <Alert severity={status} onClose={handleSnackbarClose}>{message}</Alert>
+        </Snackbar>
+      }
     </form>
   );
 }
