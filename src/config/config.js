9d816693fda23e9d385c059100c6443de655ccf3
Braian Cardozo <bcardozo@quadiontech.com>
2021-06-22 09:18:51 -0300

    Fix prod

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  baseApi: 'http://localhost:3000/api/v1'
+  // baseApi: 'http://localhost:3000/api/v1',
+  baseApi: 'https://tienda-online-pps-back.herokuapp.com//api/v1'
 };
+
+// "start": "set PORT=3006 && react-scripts start",
